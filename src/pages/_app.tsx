c76d81f761ee8b0669eaf184d8f8d3a712d0d6e6
Adrian Richard <adrichard14@hotmail.com>
2023-06-22 20:39:09 -0300

    fixing typescript types warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from '../components/layout'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: any) {
   return (
     <Layout>
       <Component {...pageProps} />
