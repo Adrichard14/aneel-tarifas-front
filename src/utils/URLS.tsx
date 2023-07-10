03ad5a4193d9fcf3ca378ab3f8a1a8928d67c219
Adrian Richard <adrichard14@hotmail.com>
2023-07-10 11:04:40 -0300

    move URLS for an utils file

---
+++ src/utils/URLS.tsx
@@ -0,0 +1,14 @@
+const URLS: any = {
+  '1': "empresas",
+  '2': "tarifas/vigenciaempresas", // /2010-01-01/2011-12-12
+  '3': "tarifas/descclasse",
+  '4': "tarifas/modalidadesregistro",
+  '5': "tarifas/valortarifapordistribuidora",
+  '6': "tarifas/numtarifasporposto",
+  '7': "subgrupos",
+  '8': "tarifas/unidadesconsumidoras",
+  '9': "tarifas/resolucoes",
+  '10': "tarifas/agentestarifas",
+};
+
+export default URLS;
